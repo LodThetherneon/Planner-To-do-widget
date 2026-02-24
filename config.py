3f211a01ec0abc775a8b1f0b8e64b45412715f40
Zoltan Kiss <141189365+LodThetherneon@users.noreply.github.com>
2026-02-24 12:24:00 +0100

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-CLIENT_ID = "34e2c374-eb9b-4a30-9f19-879117b91660" 
-
-TENANT_ID = "ba3bfa82-3437-46e6-815c-d987814eeaee"
-
-SCOPES = ['Tasks.ReadWrite', 'Tasks.Read', 'User.Read']
-
-GRAPH_API_ENDPOINT = "https://graph.microsoft.com/v1.0/me/todo/lists/tasks" 
